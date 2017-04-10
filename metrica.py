144fdb2400bd29efcf1fe10083db23224d43af62
Estafius <estafius@gmail.com>
2017-04-10 23:19:34 +0300

    Add files via upload

--- metrica.py
+++ metrica.py
@@ -1,19 +1,15 @@
-from urllib.parse import urlencode, urlparse, urljoin
-from pprint import pprint
+from urllib.parse import urljoin
 import requests
 
 
 AUTHORIZE_URL = 'https://oauth.yandex.ru/authorize'
-# APP_ID = 'ea4e1e951785496f89fbf73450961742'  # Your app_id here
 APP_ID = '4dcdbc1768e24a24a0adf924b5b24981'  # Your app_id here
 
 auth_data = {
     'response_type': 'token',
     'client_id': APP_ID
 }
-print('?'.join((AUTHORIZE_URL, urlencode(auth_data))))
 
-# TOKEN = 'AQAAAAAazgOnAAQLkuDR78i5vE9Nk_dtM7465_4'  # Your token here
 TOKEN = 'AQAAAAAUJ0LYAAQw7bg_PVCe9kjVu9ozx0O6lo0'  # Your token here
 
 
@@ -49,19 +45,15 @@ class YandexMetrika(object):
         }
 
         response = requests.get(url, params, headers=headers)
-        print(response.json())
         items_count = response.json()['data'][0]['metrics'][0]
         return items_count
 
 
-metrika = YandexMetrika(TOKEN)
+def main():
+    metrika = YandexMetrika(TOKEN)
+    list_count = ['visits', 'pageviews', 'users']
+    for counter in metrika.counter_list:
+        for option_metrica in list_count:
+            print('Number of ', option_metrica, '= ', int(metrika.get_count(counter, option_metrica)))
 
-print(YandexMetrika.__dict__)
-pprint(metrika.__dict__)
-
-print(metrika.counter_list)
-
-list_count = ['visits', 'pageviews' ,'users']
-for counter in metrika.counter_list:
-    for option_metrica in list_count:
-     print('Number of ',option_metrica, '= ', int(metrika.get_count(counter,option_metrica)))
+main()
